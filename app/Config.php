fa4920a45d09dd86bc3b3495425a3de8f5aa33c2
LuciaDiazVivero <diaz.vivero.lucia@iescamas.es>
2023-12-01 17:21:14 +0100

    Añadidos los ficheros del Integrante 3 y modificado el fichero config.txt

---
+++ app/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+ class Config
+ {
+     static public $mvc_bd_hostname = "localhost";
+     static public $mvc_bd_nombre   = "alimentos12023";
+     static public $mvc_bd_usuario  = "myjavierjf4e";
+     static public $mvc_bd_clave    = "ENH8r78e";
+     static public $mvc_vis_css     = "estilo.css";
+ }
+?>
