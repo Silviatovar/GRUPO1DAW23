e3863723f4511e8f0f1f537367b46c0a71ea9195
Javier <151174912+JavierJF20@users.noreply.github.com>
2023-12-06 16:51:12 +0100

    Update Config.php

--- app/Config.php
+++ app/Config.php
@@ -4,8 +4,8 @@
  {
      static public $mvc_bd_hostname = "localhost";
      static public $mvc_bd_nombre   = "alimentos12023";
-     static public $mvc_bd_usuario  = "myjavierjf4e";
-     static public $mvc_bd_clave    = "ENH8r78e";
+     static public $mvc_bd_usuario  = "mygrupo1da13";
+     static public $mvc_bd_clave    = "2knEB8k6";
      static public $mvc_vis_css     = "estilo.css";
  }
 ?>
